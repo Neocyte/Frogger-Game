ceb89efd50f0596962bcb4612fae7f0a5e67879e
Mason Wu <masonwu8462@yahoo.com>
2018-05-13 14:53:24 -0400

    Style: Format multi-line comments

--- js/app.js
+++ js/app.js
@@ -6,12 +6,12 @@ class Enemy {
     this.sprite = 'images/enemy-bug.png'; // Enemy image
   }
 
-  // Update the enemy's position, required method for game
-  // Parameter: dt, a time delta between ticks
+  /* Update the enemy's position, required method for game
+     Parameter: dt, a time delta between ticks */
   update(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    /* You should multiply any movement by the dt parameter
+       which will ensure the game runs at the same speed for
+       all computers */
   }
 
   // Draw the enemy on the screen, required method for game
@@ -21,9 +21,9 @@ class Enemy {
 
 }
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+/* Now write your own player class
+   This class requires an update(), render() and
+   a handleInput() method. */
 class Player {
   constructor(x, y) {
     this.x = x;
@@ -48,13 +48,13 @@ class Player {
 
 }
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+/* Now instantiate your objects.
+   Place all enemy objects in an array called allEnemies
+   Place the player object in a variable called player */
 
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+/* This listens for key presses and sends the keys to your
+   Player.handleInput() method. You don't need to modify this. */
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
