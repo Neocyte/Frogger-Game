2927236ac067dc64002a4bc35080f38f58c0f8a4
Mason Wu <masonwu8462@yahoo.com>
2018-05-18 21:44:26 -0400

    Style: Immediately invoke global 'keyListener' function

--- js/app.js
+++ js/app.js
@@ -7,6 +7,21 @@ let updateScore = function() {
   score += 1;
 };
 
+/* This listens for key presses and sends the keys to your
+   Player.handleInput() method. Immediately invoked. */
+let keyListener = (function() {
+  document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+  });
+})();
+
 // Enemies our player must avoid
 class Enemy {
   constructor() {
@@ -100,16 +115,3 @@ const allEnemies = [
 ];
 
 const player = new Player();
-
-/* This listens for key presses and sends the keys to your
-   Player.handleInput() method. */
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
-
-    player.handleInput(allowedKeys[e.keyCode]);
-});
